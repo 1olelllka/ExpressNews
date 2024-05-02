e9f761ff305dc7c96d8fd56bb9c161f065f6565c
Oleshka <olehit32@gmail.com>
2024-05-02 21:29:31 +0200

    Added saving articles through the cache. Also implemented the mechanism, if cache is empty, but db has some records

--- backend/src/middlewares/savedArticles.js
+++ backend/src/middlewares/savedArticles.js
@@ -1,37 +1,79 @@
-const mongoose = require("mongoose");
 const discordUser = require("../databases/schemas/discordUser");
 const googleUser = require("../databases/schemas/googleUser");
 const User = require("../databases/schemas/localUser");
 const Story = require("../databases/schemas/Story");
+const client = require("../databases/redis");
 
-async function addToSavedArticles(req, res, next) {
+async function showSavedStories(req, res) {
   const userId = req.user._id;
-
-  const storyId = req.body.storyId;
-  const story = await Story.findById(storyId);
-  if (!story) {
-    return res.status(404).json({ message: "Story not found" });
+  try {
+    const cached = await client.hGet(userId.toString(), "stories");
+    if (cached) {
+      const result = await Story.find({ _id: { $in: JSON.parse(cached) } });
+      return res.send(result);
+    } else {
+      const dbUser =
+        (await User.findById(userId)) ||
+        (await discordUser.findById(userId)) ||
+        (await googleUser.findById(userId));
+      const saved_stories = dbUser.saved_stories;
+      await client.hSet(
+        userId.toString(),
+        "stories",
+        JSON.stringify(saved_stories)
+      );
+      const result = await Story.find({ _id: { $in: saved_stories } });
+      res.send(result);
+    }
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
   }
-  const isAlreadySaved =
+}
+
+async function saveStory(req, res) {
+  const userId = req.user._id;
+  const { storyId } = req.body;
+  const user =
     (await User.findById(userId)) ||
     (await discordUser.findById(userId)) ||
     (await googleUser.findById(userId));
-  console.log(isAlreadySaved);
+  try {
+    await user.saved_stories.push(storyId);
+    await user.save();
 
-  if (isAlreadySaved.saved_stories.includes(storyId)) {
-    return res.status(400).json({ message: "Already saved" });
+    await client.hSet(
+      userId.toString(),
+      "stories",
+      JSON.stringify(user.saved_stories)
+    );
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
   }
-  (await User.findByIdAndUpdate(userId, {
-    $push: { saved_stories: storyId },
-  })) ||
-    (await discordUser.findByIdAndUpdate(userId, {
-      $push: { saved_stories: storyId },
-    })) ||
-    (await googleUser.findByIdAndUpdate(userId, {
-      $push: { saved_stories: storyId },
-    }));
-  res.sendStatus(200);
-  next();
 }
 
-module.exports = { addToSavedArticles };
+async function deleteStory(req, res) {
+  const userId = req.user._id;
+  const { storyId } = req.body;
+  const user =
+    (await User.findById(userId)) ||
+    (await discordUser.findById(userId)) ||
+    (await googleUser.findById(userId));
+  try {
+    await user.saved_stories.pull(storyId);
+    await user.save();
+    await client.hSet(
+      userId.toString(),
+      "stories",
+      JSON.stringify(user.saved_stories)
+    );
+    res.sendStatus(202);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+}
+
+module.exports = { showSavedStories, saveStory, deleteStory };
--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,6 +1,9 @@
 const { Router } = require("express");
-const { authenticate } = require("../middlewares/authentication");
-const { addToSavedArticles } = require("../middlewares/savedArticles");
+const {
+  showSavedStories,
+  saveStory,
+  deleteStory,
+} = require("../middlewares/savedArticles");
 const client = require("../databases/redis");
 
 const routes = Router();
@@ -14,7 +17,9 @@ routes.get("/searches", async (req, res) => {
   res.send(searches);
 });
 
-// Will be tested when frontend is ready
-routes.post("/articles/save", addToSavedArticles);
+// Saved Articles
+routes.get("/saved-articles", showSavedStories);
+routes.post("/save-article", saveStory);
+routes.delete("/delete-article", deleteStory);
 
 module.exports = routes;
