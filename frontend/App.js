0fb26fb4558fa5a91828c719e2497b8e9aea7e87
Oleshka <olehit32@gmail.com>
2024-06-05 11:56:53 +0200

    frontend

--- frontend/App.js
+++ frontend/App.js
@@ -1,59 +1,57 @@
+// import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, Text, View, FlatList } from "react-native";
+// import { useEffect, useState } from "react";
+// import { io } from "socket.io-client";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import AppNavigation from "./components/appNavigation";
 
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, FlatList } from "react-native";
-import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import AppNavigation from './components/appNavigation';
-
-
-const socket = io("http://localhost:8000/", {
-  auth: { userId: "663f3be20fc92fb910ff3567" }, // Needs to be changed
-});
-console.log(socket.auth);
+// const socket = io("https://localhost:8000/", {
+//   auth: { userId: "663f3be20fc92fb910ff3567" }, // Needs to be changed
+// });
+// console.log(socket.auth);
 export default function App() {
-  const [news, setNews] = useState([]);
+  // const [news, setNews] = useState([]);
 
-  useEffect(() => {
-    socket.on("breaking_news", (data) => {
-      console.log(data);
-      setNews((previous) => [...previous, data]);
-    });
-    return () => socket.disconnect();
-  }, []);
+  // useEffect(() => {
+  //   socket.on("breaking_news", (data) => {
+  //     console.log(data);
+  //     setNews((previous) => [...previous, data]);
+  //   });
+  //   return () => socket.disconnect();
+  // }, []);
 
-  useEffect(() => {
-    fetch("http://localhost:8000/api/v1/home/notifications/", {
-      method: "GET",
-      headers: {
-        Authorization:
-          "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NjNmM2JlMjBmYzkyZmI5MTBmZjM1NjciLCJpYXQiOjE3MTU0MjA3ODYsImV4cCI6MTcxNTQyNDM4Nn0.AC6H9KcHym2jr793c07c8AAcOdOcDMIXB5WB8wskhvg",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (news.length == 0) {
-          setNews(data);
-        }
-      });
-  }, []);
-  const renderItem = ({ item }) => (
-    <View style={{ borderBottomWidth: 1, padding: 10 }}>
-      <Text>{item.title}</Text>
-    </View>
-  );
+  // useEffect(() => {
+  //   fetch("http://localhost:8000/api/v1/home/notifications/", {
+  //     method: "GET",
+  //     headers: {
+  //       Authorization:
+  //         "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NjNmM2JlMjBmYzkyZmI5MTBmZjM1NjciLCJpYXQiOjE3MTU0MjA3ODYsImV4cCI6MTcxNTQyNDM4Nn0.AC6H9KcHym2jr793c07c8AAcOdOcDMIXB5WB8wskhvg",
+  //     },
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       if (news.length == 0) {
+  //         setNews(data);
+  //       }
+  //     });
+  // }, []);
+  // const renderItem = ({ item }) => (
+  //   <View style={{ borderBottomWidth: 1, padding: 10 }}>
+  //     <Text>{item.title}</Text>
+  //   </View>
+  // );
   return (
     <SafeAreaProvider>
       <AppNavigation />
     </SafeAreaProvider>
-  )
-    // <View>
-    //   <FlatList
-    //     data={news}
-    //     renderItem={renderItem}
-    //     keyExtractor={(item) => item.id} // Replace with unique identifier for each news item
-    //   />
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
+  );
+  // <View>
+  //   <FlatList
+  //     data={news}
+  //     renderItem={renderItem}
+  //     keyExtractor={(item) => item.id} // Replace with unique identifier for each news item
+  //   />
+  //   <Text>Open up App.js to start working on your app!</Text>
+  //   <StatusBar style="auto" />
+  // </View>
 }
--- frontend/components/screens/home/articleDetails.jsx
+++ frontend/components/screens/home/articleDetails.jsx
@@ -1,78 +1,131 @@
-import { View, Text, Image, ScrollView, FlatList, TouchableOpacity } from 'react-native'
-import React, {useState} from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { Ionicons, MaterialIcons } from '@expo/vector-icons';
-import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from "react-native-responsive-screen";
 
 export default function ArticleDetails() {
-    const [thumbsUpPressed, setThumbsUpPressed] = useState(false);
-    const [thumbsDownPressed, setThumbsDownPressed] = useState(false);
-    const changeThumbsUpPressed = () => {
-        setThumbsUpPressed(!thumbsUpPressed);
-        setThumbsDownPressed(false);
-    }
-    const changeThumbsDownPressed = () => {
-        setThumbsDownPressed(!thumbsDownPressed);
-        setThumbsUpPressed(false);
-    }
+  const [thumbsUpPressed, setThumbsUpPressed] = useState(false);
+  const [thumbsDownPressed, setThumbsDownPressed] = useState(false);
+  const [article, setArticle] = useState({});
+  const changeThumbsUpPressed = () => {
+    setThumbsUpPressed(!thumbsUpPressed);
+    setThumbsDownPressed(false);
+  };
+  const changeThumbsDownPressed = () => {
+    setThumbsDownPressed(!thumbsDownPressed);
+    setThumbsUpPressed(false);
+  };
+  useEffect(() => {
+    fetch("http://localhost:8000/api/v1/home/story/66602dd0ffa57146372441bf", {
+      method: "GET",
+      headers: {
+        Authorization:
+          "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NjU2MWFjZTQ1ZTZiZmMzZDY1ZTZmNzciLCJ1c2VybmFtZSI6IjFvbGVsbGxrYSIsImlhdCI6MTcxNzU3ODI1NSwiZXhwIjoxNzE3NTgxODU1fQ.JIn5Ro7gqtGqcbpGm1n3be4EVKqwd6j1ez31EYi6Dx0",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setArticle(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <SafeAreaView>
-        <ScrollView>
-            <View className = "" style = {{}}>
-                <View className = "flex mx-6 mt-2 flex-row items-center justify-between pb-3">                 
-                    <TouchableOpacity>
-                        <Ionicons name="close" size={30} color="black" />                  
-                    </TouchableOpacity> 
-                    <Text className = "text-neutral-500">sporty.co</Text>              
-                    <TouchableOpacity>
-                        <Ionicons name="share-outline" size={24} color="black" />
-                    </TouchableOpacity>
-                </View>
-                <View className = "border-neutral-300" style = {{borderBottomWidth: 1}} />
-                <View className = "m-4 rounded-md justify-center items-center" style = {{borderWidth: 1 ,height: hp(3.5), width: wp(15)}}>
-                    <Text className = "font-medium text-xs">SPORT</Text>
-                </View>
-                <View className = 'mx-4'>
-                    <Text className = "text-2xl font-bold">Celebrations erupt globally as underdog team's triumph</Text>
-                    <View className = 'mt-4 flex-row items-center justify-between'>
-                        <Text>By <Text className = 'font-semibold'>Layla Phillips</Text></Text>
-                        <Text className = 'text-neutral-500'>May 31, 2020</Text>
-                    </View>
-                    <View className = "mt-4">
-                        <Image className = "w-full rounded-lg" source = {require('../../../assets/images/searchResult/test_image.png')} />
-                        <Text className = "mt-2 text-neutral-500">The historic moment in the final minutes - Photo credit: JOSHUA DAVIS/ SPORTY IMAGES</Text>
-                    </View>
-                    <View className = "mt-4">
-                        <Text className = "text-base">
-                            Tempor dolore anim culpa cupidatat ex ipsum amet aliqua et magna. Pariatur qui irure aliquip velit ut duis adipisicing. Qui consectetur ut et cupidatat ipsum consequat mollit deserunt dolore.
-
-                            Exercitation nulla cupidatat consectetur voluptate. Sunt aliqua esse consectetur aute laborum aliquip culpa occaecat. Laborum nulla exercitation ut nulla sunt sunt reprehenderit occaecat consequat. Non labore ut deserunt adipisicing cupidatat esse commodo laboris irure irure nostrud. Nulla duis anim exercitation aliquip Lorem sunt amet proident qui eiusmod. Id cillum anim ut commodo. Exercitation amet quis magna ea proident et aliquip eiusmod id ipsum exercitation est cupidatat.
-
-                            Sint eu amet cupidatat irure qui qui aliqua adipisicing mollit sunt laboris aliquip est. Nulla ex et labore ad sunt cupidatat. Duis enim quis officia non labore consequat eu nisi mollit adipisicing voluptate do qui.
-                        </Text>
-                    </View>
-                </View>
+      <ScrollView style={{ height: hp(100) }}>
+        <View>
+          <View className="flex mx-6 mt-2 flex-row items-center justify-between pb-3">
+            <TouchableOpacity>
+              <Ionicons name="close" size={30} color="black" />
+            </TouchableOpacity>
+            <Text className="text-neutral-500">{article.source?.name}</Text>
+            <TouchableOpacity>
+              <Ionicons name="share-outline" size={24} color="black" />
+            </TouchableOpacity>
+          </View>
+          <View
+            className="border-neutral-300"
+            style={{ borderBottomWidth: 1 }}
+          />
+          <View
+            className="m-4 rounded-md justify-center items-center"
+            style={{ borderWidth: 1, height: hp(3.5), width: wp(15) }}
+          >
+            <Text className="font-medium text-xs">
+              {article.category?.toUpperCase()}
+            </Text>
+          </View>
+          <View className="mx-4">
+            <Text className="text-2xl font-bold">{article.title}</Text>
+            <View className="mt-4 flex-row items-center justify-between">
+              <Text>
+                By <Text className="font-semibold">{article.author}</Text>
+              </Text>
+              <Text className="text-neutral-500">{article.publishedAt}</Text>
             </View>
-            <View className = "border-neutral-300 mt-4" style = {{borderBottomWidth: 1}} />
-                <View className = "m-4 flex-row items-center justify-between">
-                    <View className = 'flex-row items-center space-x-6'>
-                        <TouchableOpacity onPress={() => changeThumbsUpPressed()}>
-                            <MaterialIcons name={thumbsUpPressed ? "thumb-up-alt" : "thumb-up-off-alt"} size={35} color={thumbsUpPressed ? '#EE6D33' : 'gray'} />
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={() => changeThumbsDownPressed()}>
-                            <MaterialIcons name={thumbsDownPressed ? "thumb-down-alt" : "thumb-down-off-alt"} size={35} color={thumbsDownPressed ? '#EE6D33' : 'gray'} />
-                        </TouchableOpacity>
-                    </View>
-                    <View className = "flex-row items-center space-x-6">
-                        <TouchableOpacity>
-                            <MaterialIcons name="arrow-back" size={30} color="black"  />
-                        </TouchableOpacity>
-                        <TouchableOpacity>
-                            <MaterialIcons name="arrow-forward" size={30} color="black" />
-                        </TouchableOpacity>
-                    </View>
-                </View>
-        </ScrollView>
+            <View className="mt-4">
+              <Image
+                className="rounded-lg"
+                source={{
+                  uri: article.urlToImage,
+                }}
+                style={{ width: "100%", height: "50%" }}
+              />
+              <Text className="mt-2 text-neutral-500">
+                {article.description}
+              </Text>
+            </View>
+            <View className="mt-4">
+              <Text className="text-base">{article.content}</Text>
+            </View>
+          </View>
+        </View>
+        <View
+          className="border-neutral-300 mt-4"
+          style={{ borderBottomWidth: 1 }}
+        />
+        <View className="m-4 flex-row items-center justify-between">
+          <View className="flex-row items-center space-x-6">
+            <TouchableOpacity onPress={() => changeThumbsUpPressed()}>
+              <MaterialIcons
+                name={thumbsUpPressed ? "thumb-up-alt" : "thumb-up-off-alt"}
+                size={35}
+                color={thumbsUpPressed ? "#EE6D33" : "gray"}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => changeThumbsDownPressed()}>
+              <MaterialIcons
+                name={
+                  thumbsDownPressed ? "thumb-down-alt" : "thumb-down-off-alt"
+                }
+                size={35}
+                color={thumbsDownPressed ? "#EE6D33" : "gray"}
+              />
+            </TouchableOpacity>
+          </View>
+          <View className="flex-row items-center space-x-6">
+            <TouchableOpacity>
+              <MaterialIcons name="arrow-back" size={30} color="black" />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <MaterialIcons name="arrow-forward" size={30} color="black" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
-  )
+  );
 }
